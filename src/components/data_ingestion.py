68647968212a658daa026fc67cb4a892387112a2
vwaghmare <vrudishwaghmare9712@gmail.com>
2026-07-08 14:19:33 +0530

    Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -51,6 +52,10 @@ if __name__=='__main__':
     data_transformation = DataTransformation()
 
     train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 
     
 
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
  
 import pickle 
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -15,3 +16,33 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+             return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys) 
+
